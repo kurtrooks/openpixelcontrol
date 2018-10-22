43db17eb562f87c12fa5e4c6e33d65ff07e02b1c
Kurt Rooks <kurt@kurtrooks.com>
2018-10-22 15:04:29 -0500

    Indexing is now organized by columns/'strands' instead of radially indexed as before

--- layouts/genTree.py
+++ layouts/genTree.py
@@ -5,29 +5,36 @@ import math
 
 ledsPerStrand = 50
 numStrands = 24
-vertSpacing = .2 # inches
-treeTopRadius = .4 # inch
-treeBaseRadius = .8 # inch
 treeCoverageAngle = 180. # deg
-treeHeight = 2  # inches
 
+# Tree dimensions, units don't really matter, just ratios
+treeTopRadius = 0.5
+treeHeight = 8.
+baseRadius = 4.
 
+stepAngle = treeCoverageAngle / numStrands
+treeHyp = math.sqrt( (baseRadius*baseRadius) + (treeHeight*treeHeight) )
+baseAngle = math.atan(treeHeight/baseRadius)
+stepHeight = (treeHyp/ledsPerStrand)*math.sin(baseAngle)
+
+# Generate evenly spaced pixels
+# Index starts at base of first column, proceeds to top of first column
+# then continues at base of second column...ending at top of last column
+pixels = []
+angle = 0.0
+for col in range(0,numStrands):
+    z = 0.0
+    for pixel in range(0,ledsPerStrand):
+        r = baseRadius - z*math.cos(baseAngle) + treeTopRadius
+        x = r*np.sin(angle*np.pi/180.)
+        y = r*np.cos(angle*np.pi/180.)
+        pixels.append( (x,y,z) )
+        z += stepHeight
+    angle += stepAngle
+
+# Write out to screen
 lines = []
-
-step = (treeBaseRadius-treeTopRadius)/ledsPerStrand
-angle = float(treeCoverageAngle) / numStrands
-
-hNum = ledsPerStrand
-tmpH = treeHeight - treeTopRadius*2 
-r = treeTopRadius
-for x in range(0,ledsPerStrand):
-    i = 0
-    while i < treeCoverageAngle:
-        lines.append('  {"point": [%.2f, %.2f, %.2f]}' %
-                (r*np.sin(i*np.pi/180.),r*np.cos(i*np.pi/180.),hNum))
-        i += angle
-
-    hNum -= (treeHeight/ledsPerStrand)
-    r += step
+for p in pixels:
+    lines.append('  {"point": [%.2f, %.2f, %.2f]}' % p )
 
 print '[\n' + ',\n'.join(lines) + '\n]'
