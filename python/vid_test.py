fe0518cd523641bd014d079d52f10cf00852e81a
Kurt <kurt@kurtrooks.com>
2018-10-30 22:00:03 -0500

    Generalizing video playback

--- python/vid_test.py
+++ python/vid_test.py
@@ -4,54 +4,79 @@ import sys
 import time
 import cv2
 import opc
+from optparse import OptionParser
+import signal
 
-#-------------------------------------------------------------------------------
-# handle command line
+run = True
 
-if len(sys.argv) == 1:
-    IP_PORT = '192.168.1.189:7890'
-elif len(sys.argv) == 2 and ':' in sys.argv[1] and not sys.argv[1].startswith('-'):
-    IP_PORT = sys.argv[1]
-else:
-    print('''
-Usage: raver_plaid.py [ip:port]
+def signal_handler(sig,frame):
+    print("k thx bye")
+    global run
+    run = False
 
-If not set, ip:port defauls to 127.0.0.1:7890
-''')
-    sys.exit(0)
+def processFrame(frame):
+    pixels = []
+    for i in range(0,24):
+        for j in range(0,50):
+            f = frame[99-(j*2)][47-(i*2)]
+            pixels.append( (f[2],f[1],f[0] ))
+    return pixels 
 
 
-#-------------------------------------------------------------------------------
-# connect to server
+signal.signal(signal.SIGINT,signal_handler)
 
-client = opc.Client(IP_PORT)
+
+parser = OptionParser()
+parser.add_option("-f","--file",dest="filename")
+parser.add_option("-i","--ip",dest="ip_port",default="192.168.1.189:7890")
+parser.add_option("-l",action="store_true",dest="loop",default=False)
+
+(options,args) = parser.parse_args()
+
+client = opc.Client(options.ip_port)
+
 if client.can_connect():
-    print('    connected to %s' % IP_PORT)
+    print('    connected to %s' % options.ip_port)
 else:
-    # can't connect, but keep running in case the server appears later
-    print('    WARNING: could not connect to %s' % IP_PORT)
-print('')
+    print('    WARNING: could not connect to %s' % options.ip_port)
 
 
-fps = 30         # frames per second
-
-cap = cv2.VideoCapture('/home/pi/Desktop/bunny_tiny.m4v')
+cap = cv2.VideoCapture(options.filename)
 
 if cap.isOpened() == False:
     print("Error opening")
+    exit()
 
-num = 0
-while cap.isOpened():
-    t = time.time() 
+frames = []
+fps  = cap.get(cv2.cv.CV_CAP_PROP_FPS)
+
+numFrames = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)) 
+for i in range(0,numFrames):
     ret,frame = cap.read()
-    pixels = []
     if ret == True:
-        for i in range(0,24):
-            for j in range(0,50):
-                f = frame[99-(j*2)][47-(i*2)]
-                pixels.append( (f[2],f[1],f[0] ))
-    client.put_pixels(pixels,channel=0)
-    while time.time() - t < (1.0/30.0):
+        frames.append(frame)
+
+cap.release()
+
+fn = 0
+first = True
+frame = None
+frmaes = []
+
+while run:
+    t = time.time()
+
+    frame = frames[fn]
+    fn += 1
+    if fn >= len(frames):
+        if options.loop:
+            fn = 0
+        else:
+            break
+
+    client.put_pixels(processFrame(frame),channel=0)
+
+    while time.time() - t < (1.0/fps):
         time.sleep(0.001)
 
 cap.release()
