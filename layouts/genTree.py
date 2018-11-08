d93c84a4a557c7da2642f98c2ad1bc6f0d2daaf8
Kurt <kurt@kurtrooks.com>
2018-11-08 15:15:08 -0600

    Adding 360 zig zag pattern tree

--- layouts/genTree.py
+++ layouts/genTree.py
@@ -3,14 +3,14 @@
 import numpy as np
 import math
 
-ledsPerStrand = 50
-numStrands = 24
-treeCoverageAngle = 180. # deg
+ledsPerStrand = 25
+numStrands = 48
+treeCoverageAngle = 360. # deg
 
 # Tree dimensions, units don't really matter, just ratios
 treeTopRadius = 0.5
-treeHeight = 8.
-baseRadius = 4.
+treeHeight = 5.
+baseRadius = 3.5
 
 stepAngle = treeCoverageAngle / numStrands
 treeHyp = math.sqrt( (baseRadius*baseRadius) + (treeHeight*treeHeight) )
@@ -30,10 +30,13 @@ for col in range(0,numStrands):
         r = baseRadius - z*math.cos(baseAngle) + treeTopRadius
         x = r*np.sin(angle*np.pi/180.)
         y = r*np.cos(angle*np.pi/180.)
-        #pixels.append( (x,y,z) )
+        col_pixels.append( (x,y,z))
         z += stepHeight
     angle += stepAngle
-    pixels += col_pixels
+    if zig and (col % 2 == 1):
+        pixels += reversed(col_pixels)
+    else:
+        pixels += col_pixels
 # Write out to screen
 lines = []
 for p in pixels:
